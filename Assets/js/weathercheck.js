7f6156b729606ce3a44c70e71a8d9e777113b10b
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-01-18 16:41:24 +0800

    chnaged query

--- Assets/js/weathercheck.js
+++ Assets/js/weathercheck.js
@@ -28,11 +28,11 @@ const getWeather = async (cityKey) => {
   // function to get id for city - set city as paramater  - to obtain weather data
   const getCity = async (city) => {
     
-    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`;
     // build query to get city id using API key and city input
-    const query = `?apikey=${key}&q=${city}`;
+    // const query = `?apikey=${key}&q=${city}`;
     // fetch resource (city id) and combine base and query together
-    const response = await fetch(base + query);
+    const response = await fetch(base);
     const data = await response.json();
     console.log(data[0]);
     return data[0];
